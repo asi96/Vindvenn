38519cadfd0b6f6086979e719cc53a99d02f7f5a
Anders Simonsen <andsim96@gmail.com>
2023-10-30 14:39:34 +0100

    Setup initial app file structure and imported assets

---
+++ vindvenn/src/components/VindvennApp/VindvennApp.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './VindvennApp.css';
+
+import clear_weather_icon from '../Assets/clear.png';
+import cloud_weather_icon from '../Assets/cloud.png';
+import drizzle_weather_icon from '../Assets/drizzle.png';
+import rainy_weather_icon from '../Assets/rain.png';
+import snowy_weather_icon from '../Assets/snow.png';
+import humidity_icon from '../Assets/humidity.png';
+import wind_icon from '../Assets/wind.png'
+import search_icon from '../Assets/search.png';
+
+const VindvennApp = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default VindvennApp
